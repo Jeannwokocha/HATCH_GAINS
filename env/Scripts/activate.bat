f26326368bea3125b7bce69c8eeede0b1e5a2479
Ayotunde1213 <aderibigbeayotunde0@gmail.com>
2024-10-03 10:35:53 +0100

    updated

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\new user\Desktop\Hatch_Gains\env
+set VIRTUAL_ENV=C:\Users\user\Desktop\HATCH_GAINS\env
 
 if not defined PROMPT set PROMPT=$P$G
 
